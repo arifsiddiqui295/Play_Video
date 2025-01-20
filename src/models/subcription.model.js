628c3812e6f06233b52d30f6e9aa233cd17dd28d
arifsiddiqui295 <arifsiddiqui2905@gmail.com>
2025-01-21 02:09:51 +0530

    Added Subscription model

---
+++ src/models/subcription.model.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema(
+  {
+    subcriber: {
+      type: Schema.Types.ObjectId, //one who is subscribing
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId, //to one whom 'subscriber' is subscribing
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Subscription = mongoose.model("Subcription", subscriptionSchema);
